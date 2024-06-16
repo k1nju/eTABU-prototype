491420334bb9dd4498327b03f4a14c430950c226
kondykov <goyalaneesh108@gmail.com>
2024-06-16 17:48:01 +0300

    ui WIP

---
+++ eTABU/eTABU/UserDataSource.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTABU
+{
+    internal class UserDataSource
+    {
+
+
+        public static readonly HttpClient client = new HttpClient();
+
+
+
+        public UserDataSource()
+        {
+            client.BaseAddress = new Uri("http://localhost:5118/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    
+
+        static async Task<int> Register(User user)
+        {
+
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "register", user);
+            response.EnsureSuccessStatusCode();
+
+            int userResponse = 0;
+            if (response.IsSuccessStatusCode)
+            {
+                userResponse = DataSerializer.Deserialize<int>(await response.Content.ReadAsStringAsync())!;
+            }
+            return userResponse;
+        }
+
+        static async Task<int> Login(User user)
+        {
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "login", user);
+            response.EnsureSuccessStatusCode();
+
+            int userResponse = 0;
+            if (response.IsSuccessStatusCode)
+            {
+                userResponse = DataSerializer.Deserialize<int>(await response.Content.ReadAsStringAsync())!;
+            }
+            return userResponse;
+        }
+    }
+}
--- eTABU/eTABUApp/MainWindow.xaml.cs
+++ eTABU/eTABUApp/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace eTABU
+namespace eTABUApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +19,6 @@ namespace eTABU
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void txtUsername_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
