bba8d884451d08c9682074267753225b9f674aed
kondykov <150603629+k1nju@users.noreply.github.com>
2024-06-20 20:59:30 +0300

    20.06

--- eTABU/eTABUApp/UserDataSource.cs
+++ eTABU/eTABUApp/UserDataSource.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Todo.Server.Utils;
 
 namespace eTABUApp
 {
---
+++ eTABU/eTABUApp/Window2.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eTABU
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public ObservableCollection<User> Users { get; set; }
+        public Window2()
+        {
+            InitializeComponent();
+            Users = new ObservableCollection<User>();
+            lstUsers.ItemsSource = Users;
+        }
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                Users.Add(new User { Name = username });
+                txtUsername.Text = "";
+            }
+        }
+
+        private void lstUsers_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (lstUsers.SelectedItem != null)
+            {
+                btnDeleteUser.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnDeleteUser.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void DeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            User selectedUser = lstUsers.SelectedItem as User;
+            Users.Remove(selectedUser);
+            btnDeleteUser.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
+
