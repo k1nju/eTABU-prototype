e09a690c6c47c51561b0e6644f0e3881284ac5e5
kondykov <goyalaneesh108@gmail.com>
2024-06-23 16:48:50 +0300

    nw commit

--- eTABU/eTABUApp/Window3.xaml.cs
+++ eTABU/eTABUApp/Window3.xaml.cs
@@ -20,14 +20,34 @@ namespace eTABUApp
     /// </summary>
     public partial class Window3 : Window
     {
+        private List<eTABU> TabuList { get; set; }
         public ObservableCollection<User> Users { get; set; }
+        public ObservableCollection<string> Words { get; set; }
+        public string MainWord { get; set; }
         public Window3(List<User> users)
         {
 
             InitializeComponent();
             Users = new ObservableCollection<User>(users);
             lstUsers.ItemsSource = Users;
+            Init();
         }
-        
+
+        private async Task Init()
+        {
+            eTABUDataSource dataSource = new eTABUDataSource();
+            TabuList = await dataSource.GetTABUs();
+            NextTabu();
+        }
+
+        private void NextTabu()
+        {
+            Random rand = new Random();
+            eTABU tmp = TabuList[rand.Next(TabuList.Count)];
+            MainWord = tmp.Mainword;
+            Words = new ObservableCollection<string>(new List<string> { tmp.Synonim1, tmp.Synonim2, tmp.Synonim3, tmp.Synonim4, tmp.Synonim5 }) ;
+
+
+        }
     }
 }
